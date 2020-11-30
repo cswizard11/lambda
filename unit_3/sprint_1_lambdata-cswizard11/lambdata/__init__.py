60ef4397da7a9a531c982ff59361ba5ef6ad93e9
Nathan Partridge <nathanpartridge11@gmail.com>
2020-11-30 15:38:15 -0700

    Fixed the lambdata functions, updated .lock files

--- unit_3/sprint_1_lambdata-cswizard11/lambdata/__init__.py
+++ unit_3/sprint_1_lambdata-cswizard11/lambdata/__init__.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import sklearn
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split as tts
 
 def null_count(df):
-    return df.isnull().value_counts().sum()
+    return df.isnull().sum().sum()
 
 def train_test_split(df, frac):
-    X_train, X_test = train_test_split(df, train_size=frac)
+    X_train, X_test = tts(df, train_size=frac)
     return X_train, X_test
